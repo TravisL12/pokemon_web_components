1da14ca3644cc1b75c6ae47124aee337c4199a23
Travis Lawrence <travis.lawrence12@gmail.com>
2022-10-06 11:59:27 -0700

    Card Image component

--- public/components/cardView/cardImage.js
+++ public/components/cardView/cardImage.js
@@ -3,20 +3,21 @@ const templateMarkup = `
     rel="stylesheet"
     href="public/components/cardView/styles.css"
   />
-  <div class="card-wrapper">
-    <h1><slot name="pokemon-name">Pikachu!</slot></h1>
-    <card-image imgSrc="https://www.freeiconspng.com/thumbs/pikachu-transparent/pikachu-transparent-hd-1.png"></card-image>
-  </div>
+  <div id="image-container"></div>
 `;
 
-class CardView extends HTMLElement {
+class CardImage extends HTMLElement {
   constructor() {
     super();
     const shadow = this.attachShadow({ mode: "open" });
     const template = document.createElement("template");
     template.innerHTML = templateMarkup;
+
+    const img = document.createElement("img");
+    img.src = this.getAttribute("imgSrc");
+    template.content.getElementById("image-container").appendChild(img);
     shadow.appendChild(template.content.cloneNode(true));
   }
 }
 
-export default CardView;
+export default CardImage;
