6758cf2fb5ebcf7460ca799654d687c33660a009
Travis Lawrence <travis.lawrence12@gmail.com>
2022-10-07 09:52:17 -0700

    setup API key

--- server/routes/cards.js
+++ server/routes/cards.js
@@ -1,9 +1,18 @@
 var express = require("express");
 var router = express.Router();
 
+const API_KEY = process.env.API_KEY;
+
 /* GET users listing. */
-router.get("/", function (req, res, next) {
-  res.json({ msg: "respond with a resource" });
+router.get("/", async function (req, res, next) {
+  const fetchCard = await fetch(
+    "https://api.pokemontcg.io/v2/cards?q=name:arceus&page=1",
+    {
+      headers: { "X-Api-Key": API_KEY },
+    }
+  );
+  const data = await fetchCard.json();
+  res.json({ msg: "respond with a resource", data });
 });
 
 module.exports = router;
